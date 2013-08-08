a9b379701cffe9b34f417bd63e94d2c719720c79
Aki Sharipov <aki@multiaki.com>
2013-08-07 22:38:26 -0400

    test

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 80);
+  app.set('port', process.env.PORT || 8080);
   app.set('views', __dirname + '/views');
   //app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -27,13 +27,6 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
-// app.get('/', routes.index);
-// app.get('/users', user.list);
-
-// app.get('/', function(req, res){
-//     // res.send('Hello');
-// });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
